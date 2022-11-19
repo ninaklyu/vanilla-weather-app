8095bd8e1436e7ec480644dc84ae2af9706ae33f
Nina <nina.klyusa@gmail.com>
2022-11-19 21:24:14 +0100

    Forecast data display using the API

--- src/app.js
+++ src/app.js
@@ -13,6 +13,44 @@ function formatDate (timestamp) {
     return `${day} ${hours}:${minutes}`;
 
 }
+function formatDay(timestamp) {
+    let date = new Date (timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+
+
+}
+function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement=document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) {
+        forecastHTML = forecastHTML + `  
+    <div class="col-2">
+        <div class="weather-forecast-day">${formatDay(forecastDay.dt)}
+        </div>
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" id="icon-forecast" width="45"/>
+        <div class="weather-forecast-temperatures">
+        <span class="temperature-max">${Math.round(forecastDay.temp.max)}° </span><span class="temperature-min"> ${Math.round(forecastDay.temp.min)}°</span>
+        </div>      
+    </div>
+    `;
+      }
+    });
+    
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+console.log (coordinates);
+let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
     let cityElement=document.querySelector("#city");
     let temperatureElement=document.querySelector("#temperature");
@@ -32,6 +70,7 @@ function displayTemperature(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute ("alt", response.data.weather[0].description);
+    getForecast(response.data.coord);
 }
 function search(city) {
     let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
